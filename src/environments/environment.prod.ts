9df40a1e0ff61cd8586e60e8526447a0c2c9725c
rahzel888 <k.ferenc.attila@gmail.com>
2024-01-31 11:44:23 +0100

    feat: update-resources, add-env-for-serverless-offline

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://4tlyd3mh61.execute-api.us-east-1.amazonaws.com',
+    bff: 'https://c98pjdrhu9.execute-api.us-east-1.amazonaws.com',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
